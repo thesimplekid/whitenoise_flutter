05f4d8f143b35ccdaf570a4472fb60fda3564eaf
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2025-06-05 14:46:37 +0200

    Add whitenoise crate and simple wrapper for flutter_rust_bridge (#43)
    * Add whitenoise crate and simple wrapper for flutter_rust_bridge
    
    * Update FRB
    
    * Fix FRB version in Cargo.toml

---
+++ rust/src/api/accounts.rs
@@ -0,0 +1,1 @@
+// This is where the account related functions will be
--- rust/src/api/mod.rs
+++ rust/src/api/mod.rs
@@ -1,1 +1,8 @@
-pub mod simple;
+pub mod accounts;
+
+use whitenoise::*;
+
+#[flutter_rust_bridge::frb(sync)]
+pub async fn initialize_whitenoise(config: WhitenoiseConfig) -> Result<Whitenoise, WhitenoiseError> {
+    Whitenoise::initialize_whitenoise(config).await
+}
